04ea1cb0688e6dc41500b1ab462e978408c17b59
Niccu Bagonoc ~Coy <nicbag@jpj.com>
2022-02-03 22:53:24 +0800

    DEV-0000/1/1: starter plate for react with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import Create from './pages/Create'
+import Update from './pages/Update'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <Router>
+        <Navigation/>
+        <div className="mx-2">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/create" component={Create} />
+            <Route path="/update/:id" component={Update} />
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom';
+import {Navbar,Nav,NavItem} from 'reactstrap';
+
+const Navigation = () => {
+  return (
+    <Navbar color='dark' dark>
+        <Link className='navbar-brand' to='/'>REACT-CRUD</Link>
+        <Nav>
+            <NavItem>
+                <Link className='btn btn-primary' to='/create'>Create Item</Link>
+            </NavItem>
+        </Nav>
+    </Navbar>
+  )
+};
+
+export default Navigation;

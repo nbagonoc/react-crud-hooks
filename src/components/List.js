d87003cef397efc1f9270907ed154c71fa8afb94
Niccu Bagonoc ~Coy <nicbag@jpj.com>
2022-02-07 23:54:00 +0800

    DEV-0000/1/1: Added loading functionality

--- src/components/List.js
+++ src/components/List.js
@@ -26,32 +26,34 @@ const List = () => {
   }
 
   return (
-    <Table bordered>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Weight</th>
-      <th>Size</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    {apiData.map(data=>{
-      return (
-        <tr key={data._id}>
-          <td>{data.name}</td>
-          <td>{data.weight}</td>
-          <td>{data.size}</td>
-          <td>
-            <Link to={`/update/${data._id}`} className='btn btn-primary mr-2'>Update</Link>
-            <Button color="danger" onClick={()=>onDelete(data._id)}>Delete</Button>
-            {/* <Button color="danger" onClick={()=>setID(data._id)}>Delete</Button> */}
-          </td>
+    apiData.length > 0 ? (
+      <Table bordered>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Weight</th>
+          <th>Size</th>
+          <th>Actions</th>
         </tr>
-      )
-    })}
-  </tbody>
-  </Table>
+      </thead>
+      <tbody>
+        {apiData.map(data=>{
+          return (
+            <tr key={data._id}>
+              <td>{data.name}</td>
+              <td>{data.weight}</td>
+              <td>{data.size}</td>
+              <td>
+                <Link to={`/update/${data._id}`} className='btn btn-primary mr-2'>Update</Link>
+                <Button color="danger" onClick={()=>onDelete(data._id)}>Delete</Button>
+                {/* <Button color="danger" onClick={()=>setID(data._id)}>Delete</Button> */}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+      </Table>
+    ) : <h3>Loading...</h3>
   );
 };
 

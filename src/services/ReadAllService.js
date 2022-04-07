3c6136a4843489883fd1c4a27bc1b8c9e53a0558
Niccu Bagonoc ~Coy <nicbag@jpj.com>
2022-04-07 08:28:17 +0800

    DEV-0000/1/1: fixed tabbing

--- src/services/ReadAllService.js
+++ src/services/ReadAllService.js
@@ -3,17 +3,17 @@ import axios from 'axios';
 import {API} from './Path';
 
 const ReadAllService = () => {
-    const [apiData, setApiData] = useState([]);
-    
-    const getData = () => {
-      axios
-      .get(API)
-      .then(res => {
-        setApiData(res.data)
-      })
-    }
-    
-    return {apiData, getData};
+  const [apiData, setApiData] = useState([]);
+  
+  const getData = () => {
+    axios
+    .get(API)
+    .then(res => {
+      setApiData(res.data)
+    })
+  }
+  
+  return {apiData, getData};
 };
 
 export default ReadAllService;
